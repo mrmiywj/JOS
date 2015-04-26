96278d31b063c7418919902abb5a20f0bb2eb357
mrmiywj <jsyangwenjie@gmail.com>
2015-04-26 14:00:19 +0800

    -.-' '

--- kern/trap.c
+++ kern/trap.c
@@ -360,6 +360,17 @@ page_fault_handler(struct Trapframe *tf)
     		utf = (struct UTrapframe*)(tf->tf_esp - sizeof(struct UTrapframe));
     	}
     	user_mem_assert(curenv,(void*)utf,sizeof(struct UTrapframe),PTE_U|PTE_W);
+
+    	utf->utf_eflags = tf->tf_eflags;
+    	utf->utf_eip = tf->tf_eip;
+    	utf->utf_err = tf->tf_err;
+    	utf->utf_esp = tf->tf_esp;
+    	utf->utf_fault_va = fault_va;
+    	utf->utf_regs = tf->tf_regs;
+
+    	curenv->env_tf.tf_eip = (uint32_t) curenv->env_pgfault_upcall;
+    	curenv->env_tf.tf_esp = (uint32_t) utf;
+
     }
 	// Destroy the environment that caused the fault.
 	cprintf("[%08x] user fault va %08x ip %08x\n",
