64ea98f626ccbfe4cc97ac8f9b3a073f08f0a7e1
mrmiywj <jsyangwenjie@gmail.com>
2015-04-27 14:22:56 +0800

    lab4 done

--- kern/syscall.c
+++ kern/syscall.c
@@ -350,7 +350,46 @@ static int
 sys_ipc_try_send(envid_t envid, uint32_t value, void *srcva, unsigned perm)
 {
 	// LAB 4: Your code here.
-	panic("sys_ipc_try_send not implemented");
+	struct Env* target;
+	struct PageInfo* page;
+	pte_t* pte;
+
+	if (( envid2env(envid,&target, 0)) < 0 ){
+		return -E_BAD_ENV;
+	}
+
+	target->env_ipc_from = curenv->env_id;
+	target->env_ipc_value = value;
+	if (target->env_ipc_recving == 0){
+		return -E_IPC_NOT_RECV;
+	}
+
+	target->env_ipc_recving = 0;
+	if ((uint32_t)srcva < UTOP ){
+		if ((uint32_t)srcva % PGSIZE != 0){
+			return -E_INVAL;
+		}
+		if ((perm & PTE_U) == 0 ||
+			(perm & PTE_P) == 0 ||
+			(perm & ~PTE_SYSCALL) != 0){
+			return -E_INVAL;
+		}
+		if ((page = page_lookup(curenv->env_pgdir,srcva,&pte)) == NULL){
+			return -E_INVAL;
+		}
+		if ((perm & PTE_W) && ((*pte & PTE_W) ==0)){
+			return -E_INVAL;
+		}
+		if ((page_insert(target->env_pgdir,page,target->env_ipc_dstva,perm)) != 0){
+			return -E_NO_MEM;
+		}
+		target->env_ipc_perm = perm;
+	}
+
+	target->env_status = ENV_RUNNABLE;
+
+	return 0;
+	//panic("sys_ipc_try_send not implemented");
 }
 
 // Block until a value is ready.  Record that you want to receive
@@ -368,7 +407,18 @@ static int
 sys_ipc_recv(void *dstva)
 {
 	// LAB 4: Your code here.
-	panic("sys_ipc_recv not implemented");
+
+	curenv->env_ipc_recving = 1;
+	if ((uint32_t)dstva < UTOP){
+		if ((uint32_t)dstva % PGSIZE != 0){
+			return -E_INVAL;
+		}
+		curenv->env_ipc_dstva = dstva;
+	}
+	curenv->env_tf.tf_regs.reg_eax = 0;
+	curenv->env_status = ENV_NOT_RUNNABLE;
+
+	//panic("sys_ipc_recv not implemented");
 	return 0;
 }
 
@@ -410,6 +460,10 @@ syscall(uint32_t syscallno, uint32_t a1, uint32_t a2, uint32_t a3, uint32_t a4, 
         	return sys_env_set_status(a1,a2);
         case SYS_env_set_pgfault_upcall:
         	return sys_env_set_pgfault_upcall(a1,(void*)a2);
+        case SYS_ipc_try_send:
+        	return sys_ipc_try_send(a1,a2,(void*)a3,a4);
+        case SYS_ipc_recv:
+        	return sys_ipc_recv((void*)a1);
 
         default:
         	cprintf("Error syscallno\n");
