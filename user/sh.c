4439dbb43f5b3f4b3a969da818e9c1669b131303
mrmiywj <jsyangwenjie@gmail.com>
2015-04-30 23:53:43 +0800

    .

--- user/sh.c
+++ user/sh.c
@@ -55,7 +55,16 @@ again:
 			// then close the original 'fd'.
 
 			// LAB 5: Your code here.
-			panic("< redirection not implemented");
+			fd = open(t,O_RDONLY);
+			if (fd < 0){
+				cprintf("syntax error: < not follow by word\n");
+				exit();
+			}
+			if (fd != 0){
+				dup(fd,0);
+				close(fd);
+			}
+			//panic("< redirection not implemented");
 			break;
 
 		case '>':	// Output redirection
