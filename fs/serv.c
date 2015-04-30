b4b44db802b97e58d3c8003bdb66fa83f891fe52
mrmiywj <jsyangwenjie@gmail.com>
2015-04-30 13:44:09 +0800

    ex7 done

--- fs/serv.c
+++ fs/serv.c
@@ -243,7 +243,23 @@ serve_write(envid_t envid, struct Fsreq_write *req)
 		cprintf("serve_write %08x %08x %08x\n", envid, req->req_fileid, req->req_n);
 
 	// LAB 5: Your code here.
-	panic("serve_write not implemented");
+	struct OpenFile* o;
+	int r;
+	int nbytes;
+
+	if ((r = openfile_lookup(envid,req->req_fileid,&o)) < 0){
+		return r;
+	}
+
+	nbytes = MIN(req->req_n, PGSIZE - sizeof(int) - sizeof(size_t));
+
+	if ((r = file_write(o->o_file, req->req_buf, nbytes, o->o_fd->fd_offset)) < 0){
+		return r;
+	}
+
+	o->o_fd->fd_offset += r;
+	return r;
+	//panic("serve_write not implemented");
 }
 
 // Stat ipc->stat.req_fileid.  Return the file's struct Stat to the
--- lib/file.c
+++ lib/file.c
@@ -141,7 +141,21 @@ devfile_write(struct Fd *fd, const void *buf, size_t n)
 	// remember that write is always allowed to write *fewer*
 	// bytes than requested.
 	// LAB 5: Your code here
-	panic("devfile_write not implemented");
+	int r;
+
+	uint32_t max_n = PGSIZE-(sizeof(int ) + sizeof(size_t));
+
+	n = n > max_n ? max_n:n;
+	fsipcbuf.write.req_fileid = fd->fd_file.id;
+	fsipcbuf.write.req_n = n;
+
+	memmove(fsipcbuf.write.req_buf, buf, n);
+	if ((r = fsipc(FSREQ_WRITE,NULL)) < 0)
+		return r;
+	assert (r <= n);
+	assert (r <= PGSIZE);
+	return r;
+	//panic("devfile_write not implemented");
 }
 
 static int
